b10a2ffdd8c036dd06cc4c5ff675e5febd9b41fa
carcamano <carcamano@gmail.com>
2016-01-08 16:42:07 -0200

    Chore: Improove documentation BugFix: Optional path param at Cookie.setCookie

--- src/services/cookie.ts
+++ src/services/cookie.ts
@@ -1,5 +1,14 @@
+/**
+ * Class Cookie - Holds static functions to deal with Cookies
+ */
 export class Cookie {
 
+	/**
+	 * Retrieves a single cookie by it's name
+	 *
+	 * @param  {string} name Identification of the Cookie
+	 * @returns The Cookie's value
+	 */
 	public static getCookie(name: string): string {
 		let myWindow: any = window;
 		name = myWindow.escape(name);
@@ -8,7 +17,16 @@ export class Cookie {
 		return (result === null) ? null : myWindow.unescape(result[1]);
 	}
 
-	public static setCookie(name: string, value: string, expires: number, path?: string, domain?: string) {
+	/**
+	 * Save the Cookie
+	 *
+	 * @param  {string} name Cookie's identification
+	 * @param  {string} value Cookie's value
+	 * @param  {number} expires? Cookie's expiration date in days from now. If it's undefined the cookie is a session Cookie
+	 * @param  {string} path? Path relative to the domain where the cookie should be avaiable. Default /
+	 * @param  {string} domain? Domain where the cookie should be avaiable. Default current domain
+	 */
+	public static setCookie(name: string, value: string, expires?: number, path?: string, domain?: string) {
 		let myWindow: any = window;
 		let cookieStr = myWindow.escape(name) + '=' + myWindow.escape(value) + ';';
 
@@ -27,6 +45,13 @@ export class Cookie {
 		document.cookie = cookieStr;
 	}
 
+	/**
+	 * Removes specified Cookie
+	 *
+	 * @param  {string} name Cookie's identification
+	 * @param  {string} path? Path relative to the domain where the cookie should be avaiable. Default /
+	 * @param  {string} domain? Domain where the cookie should be avaiable. Default current domain
+	 */
 	public static deleteCookie(name: string, path?: string, domain?: string) {
 		// If the cookie exists
 		if (Cookie.getCookie(name)) {
