2f575021cce0b3f6b564751af79b6776cd75c5a9
José Luis Di Biase <josx@interorganic.com.ar>
2016-07-12 15:22:22 -0300

    refactor(*): move typescript source files to  dir
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- ng2-cookies.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/services';
-
-import * as services from './src/services';
-
-export default {
-	services: services
-};
--- src/services.ts
+++
@@ -1,2 +0,0 @@
-// Export all services
-export {Cookie} from './services/cookie';
