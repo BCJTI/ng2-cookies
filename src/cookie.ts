e830e447b62ab47a92a78762e561a3c3489bb523
carcamano <carcamano@gmail.com>
2016-07-21 10:11:10 -0300

    Impl: Replaced deprecated window.escape to encodeURIComponent

--- src/cookie.ts
+++ src/cookie.ts
@@ -10,11 +10,10 @@ export class Cookie {
 	 * @returns The Cookie's value
 	 */
 	public static get(name: string): string {
-		let myWindow: any = window;
-		name = myWindow.escape(name);
+		name = encodeURIComponent(name);
 		let regexp = new RegExp('(?:^' + name + '|;\\s*' + name + ')=(.*?)(?:;|$)', 'g');
 		let result = regexp.exec(document.cookie);
-		return (result === null) ? null : myWindow.unescape(result[1]);
+		return (result === null) ? null : decodeURIComponent(result[1]);
 	}
 
 	/**
@@ -48,8 +47,7 @@ export class Cookie {
 	 * @param  {boolean} secure If true, the cookie will only be available through a secured connection
 	 */
 	public static set(name: string, value: string, expires?: number, path?: string, domain?: string, secure?: boolean) {
-		let myWindow: any = window;
-		let cookieStr = myWindow.escape(name) + '=' + myWindow.escape(value) + ';';
+		let cookieStr = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';';
 
 		if (expires) {
 			let dtExpires = new Date(new Date().getTime() + expires * 1000 * 60 * 60 * 24);
